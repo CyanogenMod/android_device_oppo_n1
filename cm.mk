5f194f3015262a55763b743c2e0eb7cc5a00e0c9
Steve Kondik <shade@chemlab.org>
2013-10-18 13:44:51 -0700

    n1: Remove build property overrides for ship

--- cm.mk
+++ cm.mk
@@ -11,5 +11,3 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_n1
 PRODUCT_DEVICE := n1
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=OPPO/oppo_130922/N1:4.2.2/JDQ39/182448:user/release-keys PRIVATE_BUILD_DESC="msm8960-user 4.2.2 JDQ39 eng.oppo.20130922.182448 release-keys"
