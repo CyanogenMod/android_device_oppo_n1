94b1557442c719130bcc3e9dc021512ce08ef8a8
Ethan Chen <intervigil@gmail.com>
2013-10-21 13:15:42 -0700

    n1: use Oppo suggested fingerprint

--- cm.mk
+++ cm.mk
@@ -11,3 +11,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_n1
 PRODUCT_DEVICE := n1
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=OPPO/OPPO_13055/N1:4.3.1/JLS36I/b020e44cc1:user/release-keys
