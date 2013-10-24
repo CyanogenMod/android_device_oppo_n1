2fdf608c113cd04e90c52ee0050b8eaa7a4be46c
Steve Kondik <shade@chemlab.org>
2013-10-23 17:31:30 -0700

    Remove TimeService now that we have framework support

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -64,7 +64,7 @@ done
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/$VENDOR/$DEVICE/overlay
 
-PRODUCT_PACKAGES += TimeService
+PRODUCT_PACKAGES += libtime_genoff
 
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
@@ -110,13 +110,12 @@ LOCAL_PATH := \$(call my-dir)
 ifeq (\$(TARGET_DEVICE),n1)
 
 include \$(CLEAR_VARS)
-LOCAL_MODULE := TimeService
+LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := oppo
-LOCAL_SRC_FILES := proprietary/app/TimeService.apk
+LOCAL_SRC_FILES := proprietary/lib/libtime_genoff.so
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := shared
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include \$(BUILD_PREBUILT)
 
 endif
