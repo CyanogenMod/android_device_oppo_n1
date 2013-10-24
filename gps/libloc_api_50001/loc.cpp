f5eae09c44e77f6f4625852233f666d25d204c20
Steve Kondik <shade@chemlab.org>
2013-10-23 17:59:50 -0700

    n1: Add Geofence support

--- gps/libloc_api_50001/loc.cpp
+++ gps/libloc_api_50001/loc.cpp
@@ -53,6 +53,8 @@ static gps_sv_status_callback gps_sv_cb = NULL;
 static void loc_cb(UlpLocation* location, void* locExt);
 static void sv_cb(GpsSvStatus* sv_status, void* svExt);
 
+static const GpsGeofencingInterface* get_geofence_interface(void);
+
 // Function declarations for sLocEngInterface
 static int  loc_init(GpsCallbacks* callbacks);
 static int  loc_start();
@@ -626,6 +628,40 @@ int loc_ulp_engine_update_criteria(UlpLocationCriteria criteria)
     return ret_val;
 }
 
+const GpsGeofencingInterface* get_geofence_interface(void)
+{
+    ENTRY_LOG();
+    void *handle;
+    const char *error;
+    typedef const GpsGeofencingInterface* (*get_gps_geofence_interface_function) (void);
+    get_gps_geofence_interface_function get_gps_geofence_interface;
+    static const GpsGeofencingInterface* geofence_interface = NULL;
+
+    dlerror();    /* Clear any existing error */
+
+    handle = dlopen ("libgeofence.so", RTLD_NOW);
+
+    if (!handle)
+    {
+        if ((error = dlerror()) != NULL)  {
+            LOC_LOGE ("%s, dlopen for libgeofence.so failed, error = %s\n", __func__, error);
+           }
+        goto exit;
+    }
+    dlerror();    /* Clear any existing error */
+    get_gps_geofence_interface = (get_gps_geofence_interface_function)dlsym(handle, "gps_geofence_get_interface");
+    if ((error = dlerror()) != NULL)  {
+        LOC_LOGE ("%s, dlsym for ulpInterface failed, error = %s\n", __func__, error);
+        goto exit;
+     }
+
+    geofence_interface = get_gps_geofence_interface();
+
+exit:
+    EXIT_LOG(%d, geofence_interface == NULL);
+    return geofence_interface;
+}
+
 /*===========================================================================
 FUNCTION    loc_get_extension
 
@@ -691,7 +727,12 @@ const void* loc_get_extension(const char* name)
      if(gps_conf.CAPABILITIES & ULP_CAPABILITY)
          ret_val = &sUlpNetworkInterface;
    }
-
+   else if (strcmp(name, GPS_GEOFENCING_INTERFACE) == 0)
+   {
+       if ((gps_conf.CAPABILITIES | GPS_CAPABILITY_GEOFENCING) == gps_conf.CAPABILITIES ){
+           ret_val = get_geofence_interface();
+       }
+   }
    else
    {
       LOC_LOGE ("get_extension: Invalid interface passed in\n");
