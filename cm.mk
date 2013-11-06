daf3ae75346d03f5a1bf7b4a049fcc20052eded4
Steve Kondik <shade@chemlab.org>
2013-11-06 13:56:59 -0800

    n1: Set the correct GMS clientid

--- cm.mk
+++ cm.mk
@@ -11,6 +11,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_n1
 PRODUCT_DEVICE := n1
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 TARGET_VENDOR_PRODUCT_NAME := OPPO_13055
 TARGET_VENDOR_DEVICE_NAME := N1
