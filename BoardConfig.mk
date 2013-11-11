9cacf375a0497fede5aba5eb4c3819fe809db212
Steve Kondik <shade@chemlab.org>
2013-11-11 13:29:10 -0800

    n1: Turn on zcache for improved I/O perf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom lpj=67677 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom lpj=67677 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 zcache
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 TARGET_KERNEL_CONFIG := cyanogenmod_n1_defconfig
 TARGET_KERNEL_SOURCE := kernel/oppo/n1
