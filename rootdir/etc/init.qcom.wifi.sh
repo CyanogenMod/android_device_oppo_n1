e24fbc7c2ab94db823e499a3ef8813b597d34d43
Steve Kondik <shade@chemlab.org>
2013-11-18 19:37:05 -0800

    n1: Fix WiFi on initial boot
     * Remove the code that waits on NV items to be ready for starting
       WCNSS. We will just start it with a random MAC and fill it in
       later.

--- rootdir/etc/init.qcom.wifi.sh
+++ rootdir/etc/init.qcom.wifi.sh
@@ -1,10 +1,19 @@
 #!/system/bin/sh
 
 # Workaround for conn_init not copying the updated firmware
-rm /data/misc/wifi/WCNSS_qcom_cfg.ini
-rm /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
+rm /data/misc/wifi/WCNSS_qcom_cfg.ini 2> /dev/null
+rm /data/misc/wifi/WCNSS_qcom_wlan_nv.bin 2> /dev/null
+
+echo `getprop ro.serialno` > /sys/devices/platform/wcnss_wlan.0/serial_number
+setprop wlan.driver.config /data/misc/wifi/WCNSS_qcom_cfg.ini
+
+#while [ ! "$(ls /data/opponvitems)" ]; do
+#    sleep 1;
+#done
 
 logwrapper /system/bin/conn_init
 
 echo 1 > /dev/wcnss_wlan
+
+start wcnss-service
 
