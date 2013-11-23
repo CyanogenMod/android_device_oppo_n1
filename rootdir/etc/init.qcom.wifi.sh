e8a48ca96bee58ce61e7e9bb14952e4d191f0040
Steve Kondik <shade@chemlab.org>
2013-11-23 07:30:58 -0800

    n1: Reload the WiFi firmware if we had to wait for NV items
     * Ensures the correct MAC is applied on first boot or wipe.

--- rootdir/etc/init.qcom.wifi.sh
+++ rootdir/etc/init.qcom.wifi.sh
@@ -7,13 +7,20 @@ rm /data/misc/wifi/WCNSS_qcom_wlan_nv.bin 2> /dev/null
 echo `getprop ro.serialno` > /sys/devices/platform/wcnss_wlan.0/serial_number
 setprop wlan.driver.config /data/misc/wifi/WCNSS_qcom_cfg.ini
 
-#while [ ! "$(ls /data/opponvitems)" ]; do
-#    sleep 1;
-#done
-
+# Configure the hardware. Set a random MAC if nvitems aren't available yet.
 logwrapper /system/bin/conn_init
 
 echo 1 > /dev/wcnss_wlan
+
+# Wait for nvitems and reconfigure if necessary (first boot)
+if [ ! "$(ls /data/opponvitems)" ]; then
+    while [ ! "$(ls /data/opponvitems)" ]; do
+        sleep 1;
+    done
+    logwrapper /system/bin/conn_init
+    echo 0 > /sys/module/wlan/parameters/con_mode
+fi
+
 
 start wcnss-service
 
