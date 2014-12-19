96467ea891c566fd2dc0c95ce83251a096cd7555
Ethan Chen <intervigil@gmail.com>
2014-12-19 15:09:48 -0800

    n1: Remove unused keylayouts
    Change-Id: I9493ab5245ccef53eb41c3c9fe29dcd795d847d9

--- n1.mk
+++ n1.mk
@@ -27,10 +27,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/atmel_mxt_ts.kl:system/usr/keylayout/atmel_mxt_ts.kl \
-    $(LOCAL_PATH)/keylayout/cyttsp-i2c.kl:system/usr/keylayout/cyttsp-i2c.kl \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/keypad_8960.kl:system/usr/keylayout/keypad_8960.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
 # Media profile
 PRODUCT_COPY_FILES += \
